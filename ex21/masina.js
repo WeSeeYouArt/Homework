73422cc219973c9b8cc2e4990e1738e4036cdd13
WeSeeYouArt <alexandrupotirlau@gmail.com>
2022-08-24 19:09:43 +0300

    updates

--- ex21/masina.js
+++ ex21/masina.js
@@ -1,24 +1,31 @@
 class Masina {
-    constructor(model, viteza) {
+    constructor(model, viteza, anFabricatie) {
         this.model = model;
         this.viteza = viteza;
+        this.anFabricatie = anFabricatie;
     }
     descriere() {
-        document.querySelector('.rezultat').textContent = (`Masina e ${this.model} si are viteza de ${this.viteza} km/h.`);
+        document.querySelector('.rezultat').textContent = (`Masina e ${this.model}, din anul ${this.anFabricatie} si are viteza de ${this.viteza} km/h.`);
     }
     accelerare() {
         this.viteza += 10;
     }
     franare() {
-        this.viteza -= 10;
+        if (this.viteza - 10 < 0) {
+            this.viteza = 0;
+        }
+        else {
+            this.viteza -= 10;
+        }
     }
 }
 
 document.querySelector('.mesaj').addEventListener('click', function() {
     const model = document.querySelector('.marcaMasina').value;
     const viteza = parseInt(document.querySelector('.vitezaMasina').value);
+    const anFabricatie = parseInt(document.querySelector('.anFabricatie').value);
 
-    masina = new Masina(model, viteza);
+    masina = new Masina(model, viteza, anFabricatie);
     masina.descriere();
 });
 
