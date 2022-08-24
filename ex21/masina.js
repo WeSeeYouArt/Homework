379cdc4135baa2374a11aa931a01e20607cfe0ec
WeSeeYouArt <alexandrupotirlau@gmail.com>
2022-08-24 14:57:56 +0300

    Update masina.js

--- ex21/masina.js
+++ ex21/masina.js
@@ -1,32 +1,36 @@
-class TipMasina {
-    constructor (masina, viteza, vitezaDupaAccelerare) {
-        this.masina = masina;
+class Masina {
+    constructor(model, viteza) {
+        this.model = model;
         this.viteza = viteza;
-        this.vitezaDupaAccelerare = vitezaDupaAccelerare;
     }
     accelerare() {
-        let vitezaDupaAccelerare = this.viteza +10;
+        this.viteza += 10;
+    }
+    franare() {
+        this.viteza -= 10;
     }
 }
 
-document.querySelector('.mesaj').addEventListener('click', function () {
-    const masina = document.querySelector('.marcaMasina').value;
-    const viteza = document.querySelector('.vitezaMasina').value;
+let masina;
 
-    document.querySelector('.rezultat').textContent = (`Masina e ${masina} si are viteza de ${viteza}km/h.`);
+document.querySelector('.mesaj').addEventListener('click', function() {
+    const model = document.querySelector('.marcaMasina').value;
+    const viteza = parseInt(document.querySelector('.vitezaMasina').value);
+
+    masina = new Masina(model, viteza);
+    document.querySelector('.rezultat').textContent = (`Masina e ${model} si are viteza de ${viteza} km/h.`);
 });
 
-document.querySelector('.accelerare').addEventListener('click', function () {
-    const masina = document.querySelector('.marcaMasina').value;
-    const viteza = document.querySelector('.vitezaMasina').value;
-
-
-    document.querySelector('.rezultat').textContent = (`Masina e ${masina} si are viteza de ${viteza}km/h.`);
+document.querySelector('.accelerare').addEventListener('click', function() {
+    if(masina) {
+        masina.accelerare();
+        document.querySelector('.rezultat').textContent = (`Masina e ${masina.model} si are viteza de ${masina.viteza} km/h.`);
+    }
 });
 
-document.querySelector('.franare').addEventListener('click', function () {
-    const masina = document.querySelector('.marcaMasina').value;
-    const viteza = document.querySelector('.vitezaMasina').value;
-
-    document.querySelector('.rezultat').textContent = (`Masina e ${masina} si are viteza de ${viteza}km/h.`);
+document.querySelector('.franare').addEventListener('click', function() {
+    if(masina) {
+        masina.franare();
+        document.querySelector('.rezultat').textContent = (`Masina e ${masina.model} si are viteza de ${masina.viteza} km/h.`);
+    }
 });
